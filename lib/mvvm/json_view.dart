00b516826416aaacbe7a832722f0dc4e45a3a36b
Soner97 <sonerkaraevli@gmail.com>
2020-10-30 19:06:24 +0300

    second screen generally done. I'am going to focus on the details.

--- lib/mvvm/json_view.dart
+++ lib/mvvm/json_view.dart
@@ -26,7 +26,7 @@ class JsonView extends JsonViewModel {
       height: 8.0,
       width: isActive ? 24.0 : 16.0,
       decoration: BoxDecoration(
-        color: isActive ? Colors.white : Colors.black,
+        color: isActive ? Colors.white : Colors.greenAccent,
         borderRadius: BorderRadius.all(Radius.circular(12)),
       ),
     );
@@ -93,8 +93,9 @@ class JsonView extends JsonViewModel {
                                 ),
                                 Expanded(
                                   child: FittedBox(
-                                      child: LabelText(
-                                          postModels[index].firtsArea)),
+                                    child:
+                                        LabelText(postModels[index].firtsArea),
+                                  ),
                                 ),
                                 SizedBox(
                                   width: context.dynamicWidth(0.20),
@@ -123,67 +124,101 @@ class JsonView extends JsonViewModel {
                                     postModels[index].firstAreaTodayStatus),
                               ],
                             ),
-                            Container(
-                              margin: context.paddingLow,
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.end,
-                                children: [
-                                  LabelTextDetail("Daha fazla ayrıntı"),
-                                  Padding(
-                                    padding: const EdgeInsets.only(left: 5.0),
-                                    child: Icon(
-                                      Icons.arrow_forward_ios,
-                                      color: Colors.white,
-                                      size: 16.0,
-                                    ),
-                                  )
-                                ],
-                              ),
-                            ),
-                            Container(
-                              padding: context.paddingLow,
-                              // margin: context.paddingLow,
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: [
-                                  LabelText("Bugün · "),
-                                  LabelText(
-                                      postModels[index].firstAreaTodayStatus),
-                                  Padding(
-                                    padding: const EdgeInsets.only(left: 130.0),
-                                    child: Row(
-                                      children: [
-                                        LabelText(
-                                            postModels[index].todayDegree),
-                                      ],
-                                    ),
-                                  )
-                                ],
-                              ),
-                            ),
-                            Container(
-                              padding: context.paddingLow,
-                              //  margin: context.paddingLow,
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: [
-                                  LabelText("Yarın · "),
-                                  FittedBox(
-                                      child: LabelText(
-                                          postModels[index].tomorrowStatus)),
-                                  Padding(
-                                    padding: const EdgeInsets.only(left: 130.0),
-                                    child: Row(
-                                      children: [
-                                        FittedBox(
-                                          child: LabelText(
-                                              postModels[index].tomorrowDegree),
+                            Column(
+                              children: [
+                                Container(
+                                  padding: context.paddingLow,
+                                  margin: context.paddingLow,
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.end,
+                                    children: [
+                                      LabelTextDetail("Daha fazla ayrıntı"),
+                                      Padding(
+                                        padding:
+                                            const EdgeInsets.only(left: 5.0),
+                                        child: Icon(
+                                          Icons.arrow_forward_ios,
+                                          color: Colors.white,
+                                          size: 14.0,
                                         ),
-                                      ],
-                                    ),
-                                  )
-                                ],
-                              ),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                                Container(
+                                  // padding: context.paddingLow,
+                                  margin: context.paddingLow,
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: [
+                                      LabelText("Bugün · "),
+                                      LabelText(postModels[index]
+                                          .firstAreaTodayStatus),
+                                      Padding(
+                                        padding:
+                                            const EdgeInsets.only(left: 130.0),
+                                        child: Row(
+                                          children: [
+                                            LabelText(
+                                                postModels[index].todayDegree),
+                                          ],
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                                Container(
+                                  //  padding: context.paddingLow,
+                                  margin: context.paddingLow,
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: [
+                                      LabelText("Yarın · "),
+                                      FittedBox(
+                                          child: LabelText(postModels[index]
+                                              .tomorrowStatus)),
+                                      Padding(
+                                        padding:
+                                            const EdgeInsets.only(left: 130.0),
+                                        child: Row(
+                                          children: [
+                                            FittedBox(
+                                              child: LabelText(postModels[index]
+                                                  .tomorrowDegree),
+                                            ),
+                                          ],
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Container(
+                                  height: context.dynamicHeight(0.06),
+                                  width: context.dynamicWidth(0.8),
+                                  child: RaisedButton(
+                                      color: Color(0xff5986bb),
+                                      child: Text(
+                                          "5 günlük hava durumu tahmini",
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .headline6
+                                              .copyWith(color: Colors.white)),
+                                      onPressed: () {},
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(30.0),
+                                        side: BorderSide(
+                                          color: Color(0xff5986bb),
+                                        ),
+                                      ),
+                                      padding: context.paddingLow),
+                                ),
+                              ],
                             ),
                           ],
                         ),
@@ -191,15 +226,146 @@ class JsonView extends JsonViewModel {
                       Padding(
                         padding: context.paddingLow,
                         child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            SizedBox(height: context.dynamicHeight(0.02)),
-                            Text(
-                              'Karaevli ile işlerinizi kolaylaştırın!',
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                Expanded(
+                                  child: Icon(
+                                    Icons.add,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                SizedBox(
+                                  width: context.dynamicWidth(0.20),
+                                ),
+                                Expanded(
+                                  child: FittedBox(
+                                      child: LabelText(
+                                          postModels[index].secondArea)),
+                                ),
+                                SizedBox(
+                                  width: context.dynamicWidth(0.20),
+                                ),
+                                Expanded(
+                                    child: Icon(
+                                  Icons.more_vert,
+                                  color: Colors.white,
+                                ))
+                              ],
                             ),
-                            SizedBox(height: context.dynamicHeight(0.02)),
-                            Text(
-                              'Gördüğünüz belgeleri hemen tarayarak "Text" haline getirin.',
+                            Column(
+                              children: [
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    TextDegree(postModels[index]
+                                        .secondAreaTodayDegree),
+                                    Container(
+                                      padding: EdgeInsets.only(bottom: 35.0),
+                                      child: LabelText("°C"),
+                                    )
+                                  ],
+                                ),
+                                LabelText(
+                                    postModels[index].firstAreaTodayStatus),
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                Container(
+                                  padding: context.paddingLow,
+                                  margin: context.paddingLow,
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.end,
+                                    children: [
+                                      LabelTextDetail("Daha fazla ayrıntı"),
+                                      Padding(
+                                        padding:
+                                            const EdgeInsets.only(left: 5.0),
+                                        child: Icon(
+                                          Icons.arrow_forward_ios,
+                                          color: Colors.white,
+                                          size: 14.0,
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                                Container(
+                                  // padding: context.paddingLow,
+                                  margin: context.paddingLow,
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: [
+                                      LabelText("Bugün · "),
+                                      LabelText(postModels[index]
+                                          .firstAreaTodayStatus),
+                                      Padding(
+                                        padding:
+                                            const EdgeInsets.only(left: 130.0),
+                                        child: Row(
+                                          children: [
+                                            LabelText(postModels[index]
+                                                .tomorrowDegree),
+                                          ],
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                                Container(
+                                  //  padding: context.paddingLow,
+                                  margin: context.paddingLow,
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: [
+                                      LabelText("Yarın · "),
+                                      FittedBox(
+                                          child: LabelText(postModels[index]
+                                              .tomorrowStatus)),
+                                      Padding(
+                                        padding:
+                                            const EdgeInsets.only(left: 130.0),
+                                        child: Row(
+                                          children: [
+                                            FittedBox(
+                                              child: LabelText(postModels[index]
+                                                  .tomorrowDegree),
+                                            ),
+                                          ],
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Container(
+                                  height: context.dynamicHeight(0.06),
+                                  width: context.dynamicWidth(0.8),
+                                  child: RaisedButton(
+                                      color: Color(0xff5986bb),
+                                      child: Text(
+                                          "5 günlük hava durumu tahmini",
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .headline6
+                                              .copyWith(color: Colors.white)),
+                                      onPressed: () {},
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(30.0),
+                                        side: BorderSide(
+                                          color: Color(0xff5986bb),
+                                        ),
+                                      ),
+                                      padding: context.paddingLow),
+                                ),
+                              ],
                             ),
                           ],
                         ),
